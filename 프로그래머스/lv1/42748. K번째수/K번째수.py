3e729b9565bb89259ce717557913fda93fef3758
Jeewon Yang <jwyang.ai@gmail.com>
2023-05-25 17:42:19 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,3 +1,7 @@
 def solution(array, commands):
     answer = []
+    for i in range(len(commands)):
+        tmp = commands[i]
+        sublist = sorted(array[tmp[0]-1:tmp[1]]) # sorted([array[i], ..., array[j]])
+        answer.append(sublist[tmp[2]-1])
     return answer
