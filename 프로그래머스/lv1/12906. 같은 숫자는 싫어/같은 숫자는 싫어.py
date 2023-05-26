5a1020381370745a8538a1ad0a2daf791d62aefd
Jeewon Yang <jwyang.ai@gmail.com>
2023-05-26 14:08:27 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 242.38 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if i == 0:
+            answer.append(arr[i])
+        elif 0 < i <= len(arr)-1:
+            if arr[i] != arr[i-1]:
+                answer.append(arr[i])
+            else: 
+                pass
+    return answer
