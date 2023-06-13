286e30db7ce645676f138beca43bea93f6fff8b0
Jeewon Yang <jwyang.ai@gmail.com>
2023-06-13 13:04:53 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -0,0 +1,25 @@
+from collections import deque
+# https://velog.io/@wodnr0710/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-LV.2-%EC%97%B0%EC%86%8D%EB%90%9C-%EB%B6%80%EB%B6%84-%EC%88%98%EC%97%B4%EC%9D%98-%ED%95%A9
+def solution(sequence, k):
+    answer = []
+    sequence += [0]
+    
+    q = deque()
+    sum = 0
+    start, end = 0, 0
+    result = len(sequence)
+    
+    for i in range(len(sequence)):
+        while sum > k:
+            temp = q.popleft()
+            sum -= temp
+            start += 1
+        if sum == k and end-start < result:
+            answer = [start, end]
+            result = end - start # length of sublist
+        
+        q.append(sequence[i]) # append sequence[i] to the right end of the queue
+        sum += sequence[i]
+        end = i
+    return answer
+    
